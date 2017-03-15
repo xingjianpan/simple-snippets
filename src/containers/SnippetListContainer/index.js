4050fea699455a5bf6daf55f706338f910c80660
deric <github@mail.xingjian.me>
2017-03-15 15:08:40 +0800

    add page navigation

--- src/containers/SnippetListContainer/index.js
+++ src/containers/SnippetListContainer/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Loading from 'react-loading';
+import { browserHistory } from 'react-router';
 import { fetchList, setIgnoreLastFetch } from '../../actions';
 import SnippetList from '../../components/SnippetList';
-import { browserHistory } from 'react-router';
+import { hideNotification } from '../../actions';
 
 class SnippetListContainer extends Component {
   componentDidMount() {
@@ -17,29 +19,77 @@ class SnippetListContainer extends Component {
   }
 
   fetchMore(url) {
-    this.props.fetchPostList(url);
+    this.props.fetchList(url);
+  }
+
+
+  renderButton() {
+    if (this.props.nextHref && this.props.prevHref) {
+      return (
+        <div>
+          <span>
+            <a href="#" onClick={() => { this.fetchMore(this.props.prevHref); }}>
+              Previous Page
+            </a>
+          </span>
+          {' '}
+          <span className="page-control">
+            <a href="#" onClick={() => { this.fetchMore(this.props.nextHref); }}>
+              Next Page
+            </a>
+          </span>
+        </div>
+      );
+    } else if (this.props.nextHref) {
+      return <a href="#" onClick={() => { this.fetchMore(this.props.nextHref); }}>Next Page</a>
+    } else if (this.props.prevHref) {
+      return <a href="#" onClick={() => { this.fetchMore(this.props.prevHref); }}>Previous Page</a>
+    }
+
+    return <p></p>;
   }
 
 
   render() {
+    if (this.props.hasErrored) {
+      return <p>Sorry, we cannot retrieve any snippets, please refresh。</p>;
+    }
+    if (this.props.isLoading) {
+      return <Loading type="bars" color="#e3e3e3" />;
+    }
+    document.title = 'Snippets Management';
     return (
-      <SnippetList
-        snippets={this.props.snippets}
-        onSnippetClick={(id) => {
-          browserHistory.push(`snippet/${id}`);
-        }}
-      />
+      <div>
+        <SnippetList
+          snippets={this.props.snippets}
+          onSnippetClick={(id) => {
+            browserHistory.push(`snippet/${id}`);
+          }}
+        />
+        {this.renderButton()}
+      </div>
     );
   }
 }
 
 const mapStateToPros = (state) => {
-  const { snippets } = state.snippet_list;
+  const { snippets, isLoading, hasErrored,
+          nextHref, prevHref, ignoreLastFetch,
+        } = state.snippet_list;
+  const { isActive, message, action } = state.notifications;
   return {
+    isLoading,
     snippets,
+    hasErrored,
+    nextHref,
+    prevHref,
+    ignoreLastFetch,
+    isActive,
+    message,
+    action,
   };
 };
 
 export default connect(mapStateToPros,
-  { fetchList, setIgnoreLastFetch })(SnippetListContainer);
+  { fetchList, setIgnoreLastFetch, hideNotification })(SnippetListContainer);
 
